3be6d5a917ade59f318e8f430b8b3f8f557f8b80
Maria <mariakhmel8@gmail.com>
2024-05-28 01:54:43 +0300

    add hw

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+    try {
+        const contacts = [];
+        fs.writeFile(PATH_DB, JSON.stringify(contacts), 'utf-8');
+
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 await removeAllContacts();
